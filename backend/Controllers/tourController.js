ee38c1a1de0eaa612cbb05d18d119f6d5a5a7c25
Park cloud <340688748@qq.com>
2021-08-19 15:54:38 +0800

    see express working with mongo DB'

--- backend/Controllers/tourController.js
+++ backend/Controllers/tourController.js
@@ -1,70 +1,37 @@
 const fs = require('fs');
+const Tour = require('../Models/tourModel');
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log('id:', val);
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'rejected',
-      messgage: 'invalid request',
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  const { name, price } = req.body;
-
-  if (!name && !price) {
-    return res.status(400).json({
-      status: 'rejected',
-      message: 'bad request',
-    });
-  }
-  next();
-};
 //Tour Route handlers
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tours: tours,
+      tours: null,
     },
   });
 };
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+exports.getTour = (req, res) => {};
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create({ ...req.body });
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour,
-    },
-  });
-};
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = {
-    id: newId,
-    ...req.body,
-  };
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'rejected',
+      data: {
+        message: 'Invalid data sent',
+      },
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
--- backend/routes/tourRoutes.js
+++ backend/routes/tourRoutes.js
@@ -6,12 +6,9 @@ const {
   getTour,
   updateTour,
   deleteTour,
-  checkID,
-  checkBody,
 } = require('../Controllers/tourController');
-router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(checkBody, createTour); //chain middleware
+router.route('/').get(getAllTours).post(createTour); //chain middleware
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
