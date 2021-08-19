fe6d4b381d1ade028a59f3f38ec391a9b233ccf6
Park cloud <340688748@qq.com>
2021-08-19 14:50:32 +0800

    connect mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({
   path: './config.env',
 });
+// connect db
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE__PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 // dotenv config should run before app init
 const app = require('./app');
 // Start the server
