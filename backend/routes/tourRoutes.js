4a863984c885f00896a31b87172b0ad58dd91e1e
Park cloud <340688748@qq.com>
2021-08-22 15:12:36 +0800

    intro to mongo aggregation pipepline

--- backend/routes/tourRoutes.js
+++ backend/routes/tourRoutes.js
@@ -7,8 +7,12 @@ const {
   updateTour,
   deleteTour,
   AliasTop5Tours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../Controllers/tourController');
 router.route('/top-5-tours').get(AliasTop5Tours, getAllTours);
+router.route('/tour-statistic').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/').get(getAllTours).post(createTour); //chain middleware
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
