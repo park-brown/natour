d426cd25dcfc15f52ee75678ac0c4b41996c64b3
Park cloud <340688748@qq.com>
2021-08-23 16:51:26 +0800

    catchAsync thunk pattern

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const morgan = require('morgan');
+const AppError = require('./utilities/AppError');
+const GlobalErrorHandler = require('./Controllers/errorController');
 const app = express();
 
 // Middlewares
@@ -16,4 +18,8 @@ app.use('/api/v1/users', userRouter);
 // User Routes
 app.use('/api/v1/tours', tourRouter);
 
+app.all('*', (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
+app.use(GlobalErrorHandler);
 module.exports = app;
