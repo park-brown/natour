dc417b23c765a1da847c65593b85610ceafdbe62
Park cloud <340688748@qq.com>
2021-09-03 13:54:29 +0800

    add environment variable to herokudashboard

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import theme from './theme';
+import { store } from './store';
+import { Provider } from 'react-redux';
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<Provider store={store}>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</Provider>,
 	document.getElementById('root')
 );
