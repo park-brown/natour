1324010d9596155d2ec531c8a397f02770460446
Park cloud <340688748@qq.com>
2021-09-15 12:07:47 +0800

    change get all review settings

--- backend/routes/reviewRoutes.js
+++ backend/routes/reviewRoutes.js
@@ -12,11 +12,13 @@ const { protect, restrictTo } = require('../Controllers/authController');
 // POST /tour/234fad4/reviews
 // GET /tour/234fad4/reviews
 const router = express.Router({ mergeParams: true }); // get access to tourdId in the tourRouter
-router.use(protect); // only login user can write an review
+
 router
   .route('/')
   .get(getAllReviews)
-  .post(restrictTo(['user']), setTourUserIds, createReview);
+  .post(protect, restrictTo(['user']), setTourUserIds, createReview);
+
+router.use(protect);
 router
   .route('/:id')
   .get(getReview)
