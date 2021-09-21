0558e8f361e8be756bd91d48a05160233b719e0b
Park cloud <340688748@qq.com>
2021-09-21 10:28:33 +0800

    configure redux persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from '@mui/material/styles';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 import { store } from './store';
 import { Provider } from 'react-redux';
+let persistor = persistStore(store);
 ReactDOM.render(
 	<Provider store={store}>
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<App />
-		</ThemeProvider>
+		<PersistGate loading={null} persistor={persistor}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<App />
+			</ThemeProvider>
+		</PersistGate>
 	</Provider>,
 	document.getElementById('root')
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import { natoursApi } from './API/natoursApi';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import AuthReducer from './Features/AuthSlice';
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+	blacklist: [natoursApi.reducerPath]
+};
+const rootReducer = combineReducers({
+	// Add the generated reducer as a specific top-level slice
+	[natoursApi.reducerPath]: natoursApi.reducer,
+	auth: AuthReducer
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-	reducer: {
-		// Add the generated reducer as a specific top-level slice
-		[natoursApi.reducerPath]: natoursApi.reducer,
-		auth: AuthReducer
-	},
+	reducer: persistedReducer,
 	// Adding the api middleware enables caching, invalidation, polling,
 	// and other useful features of `rtk-query`.
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(natoursApi.middleware)
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+			}
+		}).concat(natoursApi.middleware)
 });
 setupListeners(store.dispatch);
