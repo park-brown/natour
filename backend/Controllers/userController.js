a17e077213c1b38f6b94ff4aeed1920c1fe1df30
Park cloud <340688748@qq.com>
2021-09-23 13:03:12 +0800

    clear console

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -50,8 +50,6 @@ exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   next();
 });
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
